ad5e5c405a7c65cfb3d8e5463e98d3b78744a7f5
SHEWAN MANJANA <manjananew@gmail.com>
2025-01-02 16:23:45 +0530

    Update config.js

--- config.js
+++ config.js
@@ -9,7 +9,7 @@ SESSION_ID: process.env.SESSION_ID || "empngajR#7fAFYYil1LzjjD02PHTXz1hfu3PDDqNM
 PREFIX: process.env.PREFIX || ".",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 MODE: process.env.MODE || "public",
-AUTO_VOICE: process.env.AUTO_VOICE || "true",
+AUTO_VOICE: process.env.AUTO_VOICE || "false",
 AUTO_STICKER: process.env.AUTO_STICKER || "false",
 AUTO_REPLY: process.env.AUTO_REPLY || "false",
 AUTO_REACT: process.env.AUTO_REACT || "false",
